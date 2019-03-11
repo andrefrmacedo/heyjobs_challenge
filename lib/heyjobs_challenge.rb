e5a7fb384c6727d864cccc8655d2b07a81db7bd2
Andre Macedo <andrefrmacedo@gmail.com>
2019-03-11 21:48:42 +0000

    Initial gem setup

---
+++ lib/heyjobs_challenge.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class HeyjobsChallenge
+  def initialize(campaign); end
+
+  def call
+    # call api pass
+    # check for descrepancies
+    # return response
+  end
+end
+
+# assumptions
+# api url: https://mockbin.org/bin/fcb30500-7b98-476f-810d-463a0b8fc3df
+# Campaing is an object Model passed by argument
+# api url in .env file
+# this Service is part of an app, no need to create model, database, etc
+
+# Classes to create
+# API Wrapper with name: ExternalAPIWrapper
+# discrepancy checker with name: CheckDiscrepancy
+#
